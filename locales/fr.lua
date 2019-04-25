1b260ac53f97633fe5cc921ce5c0f5db2ab7fd0a
dolutattoo <47056777+dolutattoo@users.noreply.github.com>
2019-04-25 16:52:29 +0200

    Create fr.lua

---
+++ locales/fr.lua
@@ -0,0 +1,20 @@
+Locales['fr'] = {
+ 
+  -- Mission PNJ
+  
+  ['sjukhuset_transport'] = 'Emmenez moi à ~g~l'hopital~s~',
+  ['taking_service'] = 'Vous avez pris votre service',
+  ['start_job'] = 'Démarrer/annuler mission PNJ',
+  ['drive_search_pass'] = 'Faites un tour pour trouver des ~g~patients~s~',
+  ['customer_found'] = 'Vous avez trouvé un ~g~patient~s~ !',
+  ['client_unconcious'] = 'Votre client est ~r~décédé~s~. Trouvez-en un autre.',
+  ['arrive_dest'] = 'Vous êtes ~g~arrivé~s~ à destination.',
+  ['take_me_to_near'] = '~s~Emmenez moi à ~y~%s~s~, ~y~%s~s~',
+  ['take_me_to'] = '~s~Emmenez-moi à ~y~%s~s~',
+  ['close_to_client'] = 'Rapprochez-vous de votre patient',
+  ['return_to_veh'] = 'Veuillez retourner à votre véhicule et continuer votre travail.',
+  ['must_in_vehicle'] = 'Vous devez être dans votre ~b~Véhicule de service~s~ pour commencer la mission.',
+  ['have_earned'] = 'Vous avez gagné ~g~%s~s~',
+  ['comp_earned'] = '- L'hopital a gagné ~g~%s~s~\n- et vous ~g~%s~s~',
+  ['mission_complete'] = 'Mission ~y~terminée~s~!',  
+}
